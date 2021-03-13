04a1d81ddf3fa3e22eb69650b4bc272255e85700
AnnieBrighton <Annie4Brighton@gmail.com>
2021-03-14 06:09:11 +0900

    複数URL対応

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+OLDIFS=$IFS
+IFS=$'\t\n'
+
 for i in $(cd img ; ls -d */ ) ; do
-   URL=$(cd img/${i} ; ls *.url 2>/dev/null | head -1)
-   if [ "${URL}" == "" ] ; then
-      URL="${i/\//}"
-   else
-      URL="${URL/.url}"
-   fi
-   ./senmanga.py "https://raw.senmanga.com/${URL}" "${i/\//}"
+  for ul in $(cd "img/${i}" ; ls *.url 2>/dev/null) ; do
+    echo "$i"
+    URL="${ul/.url}"
+    ./senmanga.py "https://raw.senmanga.com/${URL}" "${i/\//}"
+  done
 done
 
+IFS=$OLDIFS
