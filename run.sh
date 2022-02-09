846238794abbf46e65c0c12223d1f43931cdf3c6
AnnieBrighton <Annie4Brighton@gmail.com>
2022-02-09 20:23:41 +0900

    改善版

--- run.sh
+++ run.sh
@@ -3,10 +3,13 @@
 OLDIFS=$IFS
 IFS=$'\t\n'
 
+while true ; do
 for i in $(cd img ; ls -1d */ ) ; do
   for ul in $(cd "img/${i}" ; ls -1 *.url 2>/dev/null) ; do
     URL="${ul/.url}"
-    echo "\"https://raw.senmanga.com/${URL}\" \"${i/\//}\""
+    echo "\"'https://raw.senmanga.com/${URL}'\" \"'${i/\//}'\""
   done
-done | tr "\n" "\0" | IFS=$OLDIFS /usr/bin/xargs -0 -P20 -L1 -I{} bash -c "./senmanga.py {}"
-
+done | IFS=${OLDIFS} /usr/bin/xargs -P20 -L1 -I{} -S4096 bash -c './senmanga.py {}'
+date
+sleep $(( 3600 * 6 ))
+done
