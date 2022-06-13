3e44b7734d774af70bf32aaa1a2838ab97057377
AnnieBrighton <Annie4Brighton@gmail.com>
2022-06-14 01:10:57 +0900

    add rawkuma

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ while true ; do
   find img* -name '*.url' 2>/dev/null | while read url ; do
     URL="$(basename ${url/.url})"
     DIR="$(dirname ${url})"
-    echo "./rawkuma.py \"'https://rawkuma.com/manga/${URL}'\" \"'${DIR}'\""
-    echo "./senmanga.py \"'https://raw.senmanga.com/${URL}'\" \"'${DIR}'\""
+    echo "./rawkuma.py \"'https://rawkuma.com/manga/${URL}'\" \"'${DIR}'\" ; ./senmanga.py \"'https://raw.senmanga.com/${URL}'\" \"'${DIR}'\""
   done | IFS=${OLDIFS} /usr/bin/xargs -P20 -L1 -I{} -S4096 bash -c '{}'
 
   date
